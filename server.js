29fb5516eb32ad002200a493c6e3980b0fd716b2
Bhavesh Bansode <bansodebhavesh2004@gmail.com>
2026-04-17 00:52:47 +0530

    fix db connection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+connectDB();
 
 dotenv.config();
 
